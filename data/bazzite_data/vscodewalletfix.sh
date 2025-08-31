8c3818a5356d2c9702e1f69f5613d16d87e1a97d
Ben Calvert <linuxg33k@proton.me>
2025-08-31 12:16:29 -0600

    Updated with a new comment.

--- data/bazzite_data/vscodewalletfix.sh
+++ data/bazzite_data/vscodewalletfix.sh
@@ -6,6 +6,8 @@ if grep -q '"password-store":"kwallet5"' /home/${USER}/.vscode/argv.json; then
 fi
 
 # Modify argv.json to include the password-store setting
+# sed command to insert the line before the closing brace and adds a tab before the text for proper JSON formatting
+
 echo "Applying VS Code Flatpak KDE Wallet fix..."
 sed -i '/}/i\\t// Password Store Setup for VS Code Flatpak on KDE Plasma' /home/${USER}/.vscode/argv.json
 sed -i '/}/i\\t"password-store":"kwallet5"' /home/${USER}/.vscode/argv.json
